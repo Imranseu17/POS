0dcdc45614808f5f928159bc8c4c1fd69e1cb0b9
imranKhan2018 <imran1995bd@gmail.com>
2019-01-23 20:54:19 +0600

    updated search page

--- app/src/main/java/com/example/root/pos/activities/HistoryActivity.java
+++ app/src/main/java/com/example/root/pos/activities/HistoryActivity.java
@@ -1,6 +1,9 @@
 package com.example.root.pos.activities;
 
+import android.app.DatePickerDialog;
 import android.graphics.drawable.Drawable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,12 +11,18 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.DatePicker;
 
 import com.example.root.pos.R;
 import com.example.root.pos.adapter.HistoryAdapter;
 import com.example.root.pos.model.SalesHistory;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 ;
 
 import butterknife.BindView;
@@ -26,6 +35,21 @@ public class HistoryActivity extends AppCompatActivity {
 
     @BindView(R.id.recycler_list)
     RecyclerView recyclerView;
+
+
+
+    @BindView(R.id.edt_start_date)
+    TextInputEditText edt_start_date;
+    @BindView(R.id.edt_end_date)
+    TextInputEditText edt_end_date;
+
+    private DatePickerDialog fromDatePickerDialog;
+    private DatePickerDialog toDatePickerDialog;
+    private SimpleDateFormat dateFormatter;
+    private String startDate = "";
+    private String endDate = "";
+
+
 
     private HistoryAdapter mAdapter;
     private ArrayList<SalesHistory> salesHistoryList = new ArrayList<>();
@@ -45,6 +69,19 @@ public class HistoryActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+        final String currentDate = new SimpleDateFormat("yyyyMMdd", Locale.getDefault()).format(new Date());
+        String currentDateFormatted = new SimpleDateFormat("MM-dd-yyyy", Locale.getDefault()).format(new Date());
+
+        startDate = currentDate;
+        endDate = currentDate;
+
+        edt_start_date.setText(currentDateFormatted);
+        edt_end_date.setText(currentDateFormatted);
+
+        dateFormatter = new SimpleDateFormat("MM-dd-yyyy");
+
+
+
 
         mAdapter = new HistoryAdapter(salesHistoryList);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
@@ -52,8 +89,68 @@ public class HistoryActivity extends AppCompatActivity {
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
 
+        setDatePickerField();
+
+        edt_start_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fromDatePickerDialog.show();
+            }
+        });
+        edt_end_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toDatePickerDialog.show();
+            }
+        });
+
+
 
     }
+
+    private void setDatePickerField() {
+
+        Calendar newCalendar = Calendar.getInstance();
+        fromDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+
+                String monthString = String.valueOf(monthOfYear+1);
+                if (monthString.length() == 1) {
+                    monthString = "0" + monthString;
+                }
+                String dayString = String.valueOf(dayOfMonth);
+                if (dayString.length() == 1) {
+                    dayString = "0" + dayString;
+                }
+                startDate = year+monthString+dayString;
+                edt_start_date.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        toDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                String monthString = String.valueOf(monthOfYear+1);
+                if (monthString.length() == 1) {
+                    monthString = "0" + monthString;
+                }
+                String dayString = String.valueOf(dayOfMonth);
+                if (dayString.length() == 1) {
+                    dayString = "0" + dayString;
+                }
+                endDate = year+monthString+dayString;
+                edt_end_date.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    }
+
 
 
 }
